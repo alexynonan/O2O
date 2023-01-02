8d1403aae29bd37a546ddfaa0c03a8c12b1fe275
Alexander Ynoñan <alex@soho.com.uy>
2023-01-02 16:33:23 -0500

    Create Proyect

---
+++ TechnicalTestFood/TechnicalTestFood/MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalTestFood
+//
+//  Created by Alexander Ynoñan H. on 2/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
